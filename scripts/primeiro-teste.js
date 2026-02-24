f83bf6088e0055d996a3d8f4db61258235ac0c1f
Yuri Dirickson <yuri.dirickson@usp.br>
2026-02-23 22:55:29 -0300

    addicionando ciclo de vida ao primeiro-teste

--- scripts/primeiro-teste.js
+++ scripts/primeiro-teste.js
@@ -3,15 +3,27 @@ import { check, sleep } from 'k6';
 
 export const options = {
   vus: 5, // Número de usuários virtuais
-  duration: '10s', // Duração do teste
+  //duration: '10s', // Duração do teste
+  iterations: 1000, // Número total de iterações
 };
 
-export default function () {
-  const res = http.get('http://localhost:3000/produtos');
+export function setup(){
+  console.log('Iniciando o teste de carga...');
+  return { baseUrl: 'http://localhost:3000' };
+}
 
+export default function (data) {
+  const res = http.get(`${data.baseUrl}/produtos`);
+
+  console.log(`Status: ${res.status}, Tempo de resposta: ${res.timings.duration}ms`);
+  
   check(res, {
     'Status=200': (r) => r.status === 200,
     'Resposta contem Produtos': (r) => r.body.includes('produtos'),
     'response time is less than 500ms': (r) => r.timings.duration < 500,
   });
+}
+
+export function teardown() {
+  console.log('Teste de carga concluído.');
 }
